51be89e1d1e1042f6f4398dc284120d7d474deb2
Karen <kjgamis@live.ca>
2018-06-26 14:33:21 -0400

    add new item function works

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -5,16 +5,35 @@ class Body extends React.Component {
       items: []
     }
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.addNewItem       = this.addNewItem.bind(this)
   }
 
   handleFormSubmit(name, description) {
     console.log(name, description)
+
+    let body = JSON.stringify({ item: {name: name, description: description} })
+    fetch('http://localhost:3000/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    })
+    .then((response) => { return response.json() })
+    // when form is submitted, addNewItem() is called to save items to db
+    .then((item)     => { this.addnewItem(item) })
+  }
+
+  addNewItem(item) {
+    this.setState({
+      items: this.state.items.concat(item)
+    })
   }
 
   componentDidMount() {
     fetch('/items.json')
-    .then((response) => {return response.json()})
-    .then((data) => {this.setState({ items: data }) });
+    .then((response) => { return response.json() })
+    .then((data)     => { this.setState({ items: data }) } );
   }
 
   render() {
