a86b4cf8d6dccf9b0dc7a03b63dc81ff4218517f
Karen <kjgamis@live.ca>
2018-06-27 22:44:42 -0400

    add footer

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -5,6 +5,10 @@ const Main = (props) => {
       <div className="container">
         <Body />
       </div>
+      <footer>
+        <h5>created by: <span>Karen Gamis</span></h5>
+        <h5>CRUD React-Rails App</h5>
+      </footer>
     </div>
   )
 }
