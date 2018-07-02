e37d0d34ee7d7037530e028cbd6b0d5122631dad
Karen <kjgamis@live.ca>
2018-07-01 23:56:39 -0400

    attempt to make enter key work for submit

--- app/assets/javascripts/components/_new_items.js.jsx
+++ app/assets/javascripts/components/_new_items.js.jsx
@@ -1,10 +1,17 @@
 const NewItems = (props) => {
   let formFields = {}
+  let onEnterPress = (e) => {
+    if(e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      this.myFormRef.submit();
+      }
+    }
+
   return(
     <div className="form">
       <div>
         <input className="input input-name"        ref={input => formFields.name = input}        placeholder="What needs to be done?" />
-        <input className="input input-description" ref={input => formFields.description = input} placeholder="Details.." />
+        <input className="input input-description" ref={input => formFields.description = input} placeholder="Details.." onKeyDown={this.onEnterPress}/>
       </div>
       <div></div>
       <button className="submit" onClick={ () => props.handleFormSubmit(formFields.name.value, formFields.description.value) } >+</button>
